58ced405eb11e8707c34c60cd694102fa45866a9
AlexDovgan <alex.dovgan@gmail.com>
2019-10-08 19:11:19 +0300

    chat 2

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,20 +1,29 @@
-
-firebase.firestore();
-function saveMessage(messageText, userName) {
-    return firebase.firestore().collection('messages').add({
+function ongetMessages(snapshot) {
+    console.log(snapshot.docs[0].data());
+}
+function ShowMessage(username, messageText,timestamp ) {
+    var MessageList = document.getElementById('messageList');
+    MessageList.innerHTML += `
+<div><span class="un">`+ username + `</span>
+<span class="tm">`+ timestamp + `</span>
+<span class="text">`+ messageText + `</span></div>`;
+}
+var db = firebase.firestore();
+var messages = db.collection('messages').get();
+messages.then(ongetMessages);
+console.log(messages);
+function saveMessage(messageText, userName, timestamp) {
+    return db.collection('messages').add({
         text: messageText,
         userName: userName,
-        time_stand: new Date()
+        time_stand: timestamp
     });
 }
-    function sendMessage() {
-        var MessageList =document.getElementById('messageList');
-        MessageList.innerHTML+=`
-        <div><span class="un">username</span>
-        <span class="tm">(timestamp)</span>
-        <span class="text">Message Text</span></div>`;
-        var Message = document.getElementById('messageText').value;
-        var Name = document.getElementById('name').value;
-        saveMessage(Message, Name);
+function sendMessage() {
+    var Message = document.getElementById('messageText').value;
+    var Name = document.getElementById('name').value;
+    var timestamp = new Date();
+    saveMessage(Message, Name, timestamp);
 
-    }
+   ShowMessage(Message, Name, timestamp);
+}
