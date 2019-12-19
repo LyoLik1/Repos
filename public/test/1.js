373892d66eb1c3c66e243f1cf35fc36a69606f4d
AlexDovgan <alex.dovgan@gmail.com>
2019-12-19 19:05:41 +0200

    firebase autencitation

--- public/test/1.js
+++ public/test/1.js
@@ -1,29 +1,41 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        recaptchaParameters: {
-          type: 'image', // 'audio'
-          size: 'normal', // 'invisible' or 'compact'
-          badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
-        },
-        defaultCountry: 'UA',
-        loginHint: '+380'
-      }
-    ]
-  });
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-     var userInfo = document.getElementById('user-info');
-     userInfo.style.display='block'
-     userInfo.innerHtml =user.phoneNumber;
-      
-    } else {
-      // User is signed out.
-      // ...
+  callbacks:{
+    signInSuccess:()=>false
+  },
+  signInOptions: [
+    {
+      provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      recaptchaParameters: {
+        type: 'image', // 'audio'
+        size: 'normal', // 'invisible' or 'compact'
+        badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
+      },
+      defaultCountry: 'UA',
+      loginHint: '+380'
     }
-    return false;
-  });
+  ]
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    var userInfo = document.getElementById('user-info');
+     userInfo.innerHTML = user.phoneNumber;
+    document.getElementById('firebaseui-auth-container').style.display = "none";
+    document.getElementById('Logout').style.display = "block";
+  
+
+
+  } else {
+    document.getElementById('firebaseui-auth-container') .style.display = "block"; 
+    document.getElementById('logout').style.display = "none";
+  
+
+  }
+  return false;
+});
+function logOut(){
+firebase.auth().signOut();
+
+}
