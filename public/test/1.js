fe377b3e435c9fa53eaa3f7035f56050bf85f643
AlexDovgan <alex.dovgan@gmail.com>
2019-12-24 19:02:39 +0200

    RECAPTURE

--- public/test/1.js
+++ public/test/1.js
@@ -1,41 +1,42 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-ui.start('#firebaseui-auth-container', {
-  callbacks:{
-    signInSuccess:()=>false
-  },
-  signInOptions: [
-    {
-      provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      recaptchaParameters: {
-        type: 'image', // 'audio'
-        size: 'normal', // 'invisible' or 'compact'
-        badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
-      },
-      defaultCountry: 'UA',
-      loginHint: '+380'
-    }
-  ]
-});
-
+function uiStart() {
+  ui.start('#firebaseui-auth-container', {
+    callbacks: {
+      signInSuccess: () => false
+    },
+    signInOptions: [
+      {
+        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        recaptchaParameters: {
+          type: 'image',
+          size: 'normal',
+          badge: 'bottomleft'
+        },
+        defaultCountry: 'UA',
+        loginHint: '+380'
+      }
+    ]
+  });
+}
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     var userInfo = document.getElementById('user-info');
-     userInfo.innerHTML = user.phoneNumber;
-    document.getElementById('firebaseui-auth-container').style.display = "none";
-    document.getElementById('Logout').style.display = "block";
-  
+    userInfo.innerHTML = user.phoneNumber
+    // document.getElementById('firebaseui-auth-container').style.display = "block";
+    document.getElementById('logout').style.display = "block";
+    document.getElementById('user-info').style.display = "block";
 
 
   } else {
-    document.getElementById('firebaseui-auth-container') .style.display = "block"; 
+    //document.getElementById('firebaseui-auth-container') .style.display = "none"; 
     document.getElementById('logout').style.display = "none";
-  
-
+    document.getElementById('user-info').style.display = "none";
+    uiStart();
   }
   return false;
 });
-function logOut(){
-firebase.auth().signOut();
-
+function logOut() {
+  firebase.auth().signOut();
+  
 }
